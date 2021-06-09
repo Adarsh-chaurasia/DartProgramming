4454ec1f0d084ff6f60a7545585aebfbcfe5beed
Adarsh-chaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-06-09 07:43:39 +0100

    Create TernaryOperators.dart

---
+++ Day1/TernaryOperators.dart
@@ -0,0 +1,15 @@
+
+void main(){
+  var name="adarsh";
+  var nametoprint=name??"Invalid";
+  
+  print(nametoprint);
+
+
+
+  var name;
+  var nametoprint=name??"Invalid";
+  
+  print(nametoprint);
+
+}
