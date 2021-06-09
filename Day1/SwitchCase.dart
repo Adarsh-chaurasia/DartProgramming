c4005d94212ea32b7c77dac2b36a5fd106c130e6
Adarsh-chaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-06-09 07:45:48 +0100

    Create SwitchCase.dart

---
+++ Day1/SwitchCase.dart
@@ -0,0 +1,17 @@
+void main(){
+  
+  var grade="A";
+  
+  switch(grade){
+    case 'A':
+      print("You got A");
+      break;
+    case 'B':
+      print("You got B");
+      break;
+      
+    default:
+      print("You are fail");
+        
+  }
+}
