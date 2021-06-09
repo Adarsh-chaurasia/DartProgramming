49ede6ea99cc135c57167786860a0bb1a6f36d61
Adarsh-chaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-06-09 07:11:49 +0100

    Create Comments.dart

---
+++ Day1/Comments.dart
@@ -0,0 +1,17 @@
+void main(){
+  
+  //this is a cooment in a dart
+  
+  for (int i=0;i<100;i++){
+    if (i&1 == 1 ){
+      print("Odd numbers");
+    }
+    else{
+      print("Even numbers");
+    }
+  }
+  
+  
+  
+  
+}
