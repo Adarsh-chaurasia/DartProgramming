fb9d1502757cd7d3ba544a6044aea99d6dda69e4
Adarsh-chaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-06-11 05:24:26 +0100

    Create ShortHandSyntax.dart

---
+++ day2/ShortHandSyntax.dart
@@ -0,0 +1,17 @@
+
+
+//ShortHandSyntax
+void main(){
+  
+  findperimeter(4,3);
+  area(4,5);
+}
+
+
+
+void findperimeter(int a,int b)=>
+  print("The Perimeter of rectangle is ${2*(a+b)}");
+
+
+area(int a,int b)=>print("The area of rectangle is ${a*b}");
+   
