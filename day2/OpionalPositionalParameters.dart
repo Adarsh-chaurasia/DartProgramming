ede9ab448f127a65c27103555feb805aee737ca9
Adarsh-chaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-06-11 05:35:30 +0100

    Create OpionalPositionalParameters.dart

---
+++ day2/OpionalPositionalParameters.dart
@@ -0,0 +1,22 @@
+
+void main(){
+ printcities("NewYork","newDelhi","Mumbai");
+  print("");
+printCountry("USA");
+
+}
+
+
+
+printcities(var s1,var s2,var s3){print("City1 is $s1");
+print("city2 is $s2");
+print("city3 is $s3");
+                                 }
+
+
+
+//OptionalPositionalParameters
+printCountry(var s1,[var s2,var s3]){print("Country1 is $s1");
+print("Country2 is $s2");
+print("Country3 is $s3");
+                                 }
